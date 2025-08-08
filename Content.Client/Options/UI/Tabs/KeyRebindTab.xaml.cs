693d593a22ea2c9ccdb7a3adf43c5684594100e1
Krosus777 <38509947+Krosus777@users.noreply.github.com>
2025-08-08 16:27:32 +0200

    feat: expose form radial menu keybind

--- Content.Client/Options/UI/Tabs/KeyRebindTab.xaml.cs
+++ Content.Client/Options/UI/Tabs/KeyRebindTab.xaml.cs
@@ -221,6 +221,7 @@ namespace Content.Client.Options.UI.Tabs
             AddButton(ContentKeyFunctions.OpenAHelp);
             AddButton(ContentKeyFunctions.OpenActionsMenu);
             AddButton(ContentKeyFunctions.OpenEmotesMenu);
+            AddButton(ContentKeyFunctions.OpenPaperRadialMenu);
             AddButton(ContentKeyFunctions.ToggleRoundEndSummaryWindow);
             AddButton(ContentKeyFunctions.OpenEntitySpawnWindow);
             AddButton(ContentKeyFunctions.OpenSandboxWindow);
